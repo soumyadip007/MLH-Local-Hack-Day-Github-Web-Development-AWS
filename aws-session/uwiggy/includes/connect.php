1caaa89909aab53e10dfde6ca44d31a1bf6bf653
soumyadip007 <soumyadip.note@gmail.com>
2019-10-11 21:38:55 +0530

    uiggy database @soumyadip007

--- aws-session/uwiggy/includes/connect.php
+++ aws-session/uwiggy/includes/connect.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-$servername = "localhost";
-$server_user = "root";
-$server_pass = "";
-$dbname = "food";
+$servername = "remotemysql.com";
+$server_user = "VHXyDfVav2";
+$server_pass = "OpSezSA1FR";
+$dbname = "VHXyDfVav2";
 $name = $_SESSION['name'];
 $role = $_SESSION['role'];
 $con = new mysqli($servername, $server_user, $server_pass, $dbname);
